5d44c93ce1c8de230037073d2b951281c50700cb
Larissa Muniz <larissampmuniz@gmail.com>
2025-05-05 15:32:38 -0300

    Adiciona projeto VisionHive desenvolvido por spring boot

---
+++ Sprint.1/Java/VisionHive/src/main/java/com/br/fiap/vision_hive/VisionHiveApplication.java
@@ -0,0 +1,13 @@
+package com.br.fiap.vision_hive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionHiveApplication.class, args);
+	}
+
+}
---
+++ Sprint.1/Java/VisionHive/src/test/java/com/br/fiap/vision_hive/VisionHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.fiap.vision_hive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionHiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
